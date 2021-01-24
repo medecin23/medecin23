b63b86d68fd76da56b30dbc429a7016dac30aa42
Code it <oussama2120_76@yahoo.com>
2021-01-24 21:34:03 +0100

    udapte

--- effect.js
+++ effect.js
@@ -1,2 +1,14 @@
-/* global console, tilt*/
-('.Button').tilt({ scale: 1.1, speed: 1000 });
+/*global console, alert*/
+
+function advice() {
+alert("I know it seems hard somtime but remember one thing");
+
+alert("Through every Dark night there's bright day after that");
+
+alert("So no matter how hard it get. stick your chest out. keep ya head up.... and handle it.");
+};
+
+function valible() {
+
+alert("Cette fonction n'est pas encore disponible")
+}
